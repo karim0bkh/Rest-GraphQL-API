13a827ba0904cb3e0dac3c18fc4bb285201e2408
karim0bkh <45471990+karim0bkh@users.noreply.github.com>
2023-05-03 12:37:12 +0100

    Added update one or more fields

--- userResolver.js
+++ userResolver.js
@@ -56,27 +56,47 @@ deleteUser: async ({ id }) => { // Use async/await to handle asynchronous databa
       throw err;
     }
   },
-  updateUser: async ({ id, name, email, password }) => { // Use async/await to handle asynchronous database operations
+  updateUser: async ({ id, name, email, password }) => {
     try {
-      const user = await userResolver.user({ id }); // Get the user with the given ID
+      const user = await userResolver.user({ id });
       if (!user) {
         throw new Error(`User with id ${id} not found.`);
       }
-      await new Promise((resolve, reject) => { // Use a promise to handle the asynchronous database operation
-        db.run(`UPDATE users SET name = ?, email = ?, password = ? WHERE id = ?`,
-          [name, email, password, id], function(err) {
-            if (err) {
-              reject(err);
-            } else {
-              resolve();
-            }
-          });
+      const fieldsToUpdate = {};
+      if (name) {
+        fieldsToUpdate.name = name;
+      }
+      if (email) {
+        fieldsToUpdate.email = email;
+      }
+      if (password) {
+        fieldsToUpdate.password = password;
+      }
+      const fieldKeys = Object.keys(fieldsToUpdate);
+      if (fieldKeys.length === 0) {
+        throw new Error('At least one field must be provided to update.');
+      }
+      const placeholders = fieldKeys.map((key) => `${key} = ?`).join(', ');
+      const fieldValues = fieldKeys.map((key) => fieldsToUpdate[key]);
+      fieldValues.push(id);
+      await new Promise((resolve, reject) => {
+        const sql = `UPDATE users SET ${placeholders} WHERE id = ?`;
+        console.log(sql);
+        console.log(fieldValues);
+        db.run(sql, fieldValues, function (err) {
+          if (err) {
+            reject(err);
+          } else {
+            resolve();
+          }
+        });
       });
-      return { id, name, email, password }; // Return the updated user
+      const updatedUser = { id, ...fieldsToUpdate };
+      return updatedUser;
     } catch (err) {
       throw err;
     }
   },
-};
+    };
 
 module.exports = userResolver;
